e89cdfb41eb7a5997679ae3cb434a06943d49ac1
mohammad reza <mrghkh12@gmail.com>
2024-07-09 14:24:32 +0330

    feat(script.js): create api request structure

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,19 @@
 const $ = document;
-const searchInputElem = $.querySelector(".search input")
-const searchBtn = $.querySelector(".search button")
+const searchInputElem = $.querySelector(".search input");
+const searchBtn = $.querySelector(".search button");
 
-searchBtn.addEventListener('click' , () => {
-    console.log(searchInputElem.value);
-})
+searchBtn.addEventListener("click", () => {
+  checkWeather("qazvin");
+});
 
+async function checkWeather(cityName) {
+  let apiKey = "6e9f98b15b9622b7458c9c7e66ba0194";
+  let apiUrl = "https://api.openweathermap.org/data/2.5/weather";
+
+  let responce = await fetch(
+    `${apiUrl}?q=${cityName}&appid=${apiKey}&units=metric`
+  );
+  let weatherData = await responce.json();
+
+  console.log(weatherData);
+}
